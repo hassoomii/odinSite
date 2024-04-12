d7d04035e754a0786baa02ebd94dc56261829902
husamasamarai <120605011+hassoomii@users.noreply.github.com>
2024-04-12 12:00:18 -0500

    finally added game lgic and updates based on logic in functions

--- index.js
+++ index.js
@@ -8,18 +8,56 @@ const compGamesWon = document.getElementById('compGamesWon');
 let playerChoice = 0;
 let compChoice = 0;
 
+function winnerCheck(){
+    if (parseInt(playerScore.textContent) === 5){
+        playerGamesWon.textContent ++ ;
+        playerScore.textContent = 0;
+        compScore.textContent = 0;
+    } else if (parseInt(compScore.textContent) === 5){
+        compGamesWon.textContent ++ ;
+        playerScore.textContent = 0;
+        compScore.textContent = 0;
+    }
+}
+
 function inputCheck(){
     compChoice = Math.floor((Math.random() * 3) + 1)
-    
+    if ((playerChoice === 1) && (compChoice === 2)){
+        compScore.textContent ++ ;
+        winnerCheck();
+    } else if ((playerChoice === 1) && (compChoice === 3)){
+        playerScore.textContent ++ ;
+        winnerCheck();
+    } else if ((playerChoice === 2) && (compChoice === 1)){
+        playerScore.textContent ++ ;
+        winnerCheck();
+    } else if ((playerChoice === 2) && (compChoice === 3)){
+        compScore.textContent ++ ;
+        winnerCheck();
+    } else if ((playerChoice === 3) && (compChoice === 1)){
+        compScore.textContent ++ ;
+        winnerCheck();
+    } else if ((playerChoice === 3) && (compChoice === 2)){
+        playerScore.textContent ++ ;
+        winnerCheck();
+    } else if (playerChoice === compChoice){
+        playerScore.textContent ++ ;
+        compScore.textContent ++ ;
+        winnerCheck();
+    }
 }
+
 rockButton.addEventListener('click', () => {
+    playerChoice = 1;
     inputCheck();
 });
 
 paperButton.addEventListener('click', () => {
-    playerScore.textContent ++ ;
+    playerChoice = 2;
+    inputCheck();
 });
 
 scissorButton.addEventListener('click', () => {
-    playerScore.textContent ++ ;
+    playerChoice = 3;
+    inputCheck();
 });
