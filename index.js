3b59bd4f769d3a2a45dabff463cf32336e4629f4
husamasamarai <120605011+hassoomii@users.noreply.github.com>
2024-04-12 10:51:53 -0500

    i added js functionality and console logs to check correct inputs

---
+++ index.js
@@ -0,0 +1,16 @@
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorButton = document.getElementById('scissor');
+
+// Now you can use these references to manipulate the buttons or add event listeners, for example:
+rockButton.addEventListener('click', () => {
+    console.log('rock')
+});
+
+paperButton.addEventListener('click', () => {
+    console.log('paper')
+});
+
+scissorButton.addEventListener('click', () => {
+    console.log('scissor')
+});
